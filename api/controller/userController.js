d8d5102cb527607dfcd0a265fe86db3cb61a65e7
Nguyễn Tử Toàn Lợi <kaitokid14121998@gmail.com>
2019-11-30 15:39:02 +0700

    .

--- api/controller/userController.js
+++ api/controller/userController.js
@@ -6,11 +6,15 @@ exports.createUser = function(req,res) {
 
     id_comment = (req.body.id_comment == undefined) ? null : req.body.id_comment.split(',')
     var user = new User ({
-        role : Number(req.body.role),
-        star : Number(req.body.star),
-        number_rated : Number(req.body.number_rated),
-        id_comment : id_comment,
-        name : req.body.name,
+
+        user_name: req.body.user_name,
+        password: req.body.password,
+        role: Number(req.body.role),
+        star: Number(req.body.star),
+        number_rated: Number(req.body.number_rated),
+        id_comment: id_comment,
+        name: req.body.name,
+        
     })
 
     user.save(function (err) {
--- api/schema/user.js
+++ api/schema/user.js
@@ -1,17 +1,45 @@
 var mongoose = require('mongoose');
+var bcrypt   = require('bcrypt-nodejs');
 
 var Schema = mongoose.Schema;
 
 var User = new Schema({
-    role: {type: Number, required: true},
-    star: {type: Number},
-    number_rated: {type: Number},
-    id_comment: [{type: Schema.Types.ObjectId, ref: 'Comment'}],
-    name: {type: String, required: true}
+
+    user_name: {
+        type: String,
+        require: true
+    },
+
+    password: {
+        type: String,
+        require: true
+    },
+
+    role: {
+        type: Number, 
+        required: true
+    },
+
+    star: {
+        type: Number
+    },
+
+    number_rated: {
+        type: Number
+    },
+
+    id_comment: [{
+        type: Schema.Types.ObjectId, 
+        ref: 'Comment'
+    }],
+
+    name: {
+        type: String, 
+        required: true
+    }
 })
 
-User
-    .virtual('star_avg')
+User.virtual('star_avg')
     .get(function(){
         if (this.number_rated != 0) {
             return Math.round(this.star / this.number_rated)
@@ -21,4 +49,47 @@ User
         }
     })
 
+User.pre('save', function(next){
+
+    var user = this;
+    var SALT_FACTOR = 5;
+
+    if(!user.isModified('password')){
+        return next();
+    } 
+
+    bcrypt.genSalt(SALT_FACTOR, function(err, salt){
+
+        if(err){
+            return next(err);
+        }
+
+        bcrypt.hash(user.password, salt, null, function(err, hash){
+
+            if(err){
+                return next(err);
+            }
+
+            user.password = hash;
+            next();
+
+        });
+
+    });
+
+});
+
+User.methods.comparePassword = function(passwordAttempt, cb){
+
+    bcrypt.compare(passwordAttempt, this.password, function(err, isMatch){
+
+        if(err){
+            return cb(err);
+        } else {
+            cb(null, isMatch);
+        }
+    });
+
+}
+    
 module.exports = mongoose.model('User', User);
