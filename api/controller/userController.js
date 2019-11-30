fd9f012aa1d7d62d6078a884c8aebc474e2e37a4
cuonghp2112 <cuonghp2112@gmail.com>
2019-11-30 15:50:49 +0700

    update usercontroller

--- api/controller/userController.js
+++ api/controller/userController.js
@@ -6,22 +6,17 @@ const bcrypt = require('bcrypt')
 exports.createUser = function(req,res) {
 
     id_comment = (req.body.id_comment == undefined) ? null : req.body.id_comment.split(',')
-    bcrypt.hash(req.body.password, 10, function(err, hash){ //Mã hóa mật khẩu trước khi lưu vào db
-        if (err) {return next(err);}
-        const user = new User ({
-            role : Number(req.body.role),
-            star : Number(req.body.star),
-            number_rated : Number(req.body.number_rated),
-            id_comment : id_comment,
-            name : req.body.name,
-            password = hash,
-        })
-        user.save((err, result) => {
-            if(err) {return res.json({err})}
-            res.json({user: result})
-        })
+    var user = new User ({
+        user_name: req.body.user_name,
+        password: req.body.password,
+        role: Number(req.body.role),
+        star: Number(req.body.star),
+        number_rated: Number(req.body.number_rated),
+        id_comment: id_comment,
+        name: req.body.name,
+        password: req.body.password
+        
     })
-
     user.save(function (err) {
         if (err) {return next(err);}
         console.log("Luu thanh cong");
@@ -33,20 +28,20 @@ exports.createUser = function(req,res) {
 exports.registerUser = function(req, res, next){    
     User.findOne({cmt: req.body.cmt, role: Number(req.body.role)}, (err, user) => {
         if(user == null){ //Kiểm tra xem cmt đã được sử dụng chưa
-            bcrypt.hash(req.body.password, 10, function(err, hash){ //Mã hóa mật khẩu trước khi lưu vào db
-                if (err) {return next(err);}
-                const user = new User ({
-                    role : Number(req.body.role),
-                    star : Number(req.body.star),
-                    number_rated : Number(req.body.number_rated),
-                    id_comment : id_comment,
-                    name : req.body.name,
-                    password = hash,
-                })
-                user.save((err, result) => {
-                    if(err) {return res.json({err})}
-                    res.json({user: result})
-                })
+            var user = new User ({
+                user_name: req.body.user_name,
+                password: req.body.password,
+                role: Number(req.body.role),
+                star: Number(req.body.star),
+                number_rated: Number(req.body.number_rated),
+                id_comment: id_comment,
+                name: req.body.name,
+                password: req.body.password
+                
+            })
+            user.save((err, result) => {
+                if(err) {return res.json({err})}
+                res.json({user: result})
             })
         }else{
             res.json({err: 'Chung minh thu da duoc dung'})
