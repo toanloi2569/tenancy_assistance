3b8520ef30d93a25ea3af3253781d01f76a6c3b3
Nguyễn Tử Toàn Lợi <kaitokid14121998@gmail.com>
2019-12-08 12:14:55 +0700

    .

--- api/app.js
+++ api/app.js
@@ -27,12 +27,6 @@ var upload = multer({ storage: storage });
 
 // view engine setup
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
